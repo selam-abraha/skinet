50feebae69c30a119958ac4ee981e3c6b650ffcb
Selam Abraha <selamawitabraha4@gmail.com>
2025-04-29 13:55:58 -0700

    End of section 3

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using System;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,21 +9,17 @@ using Microsoft.EntityFrameworkCore.Design.Internal;
 namespace API.Controllers;
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController :ControllerBase
+public class ProductsController (IProductRepository repo):ControllerBase
 {
-    private readonly StoreContext context;
-
-    public ProductsController(StoreContext context){
-        this.context = context;
-    }
+  
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(){
-        return await context.Products.ToListAsync();
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort ){
+        return Ok(await repo.GetProductsAsync(brand, type, sort));
     }
 
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<Product>> GetProducts(int id){
-        var product= await context.Products.FindAsync(id);
+    public async Task<ActionResult<Product>> GetProduct(int id){
+        var product= await repo.GetProductByIdAsync(id);
 
         if (product == null ) return NotFound();
 
@@ -31,9 +28,11 @@ public class ProductsController :ControllerBase
 
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product){
-        context.Products.Add(product);
-        await context.SaveChangesAsync();
-        return product;
+        repo.AddProduct(product);
+        if(await repo.SaveChangesAsync()){
+            return CreatedAtAction("GetProduct", new{id=product.Id}, product);
+        }
+        return BadRequest("Problem creating product.");
     }
 
     [HttpPut("{id:int}")]
@@ -47,22 +46,34 @@ public class ProductsController :ControllerBase
         .State = EntityState.Modified: setting the entity as modified so when 
         saveChanges() is called, EFwill generate an Update statement for it
         */
-        context.Entry(product).State = EntityState.Modified;
+       repo.UpdateProduct(product);
 
-        await context.SaveChangesAsync();
+        if (await repo.SaveChangesAsync()){
+            return NoContent();
+        }
 
-        return NoContent();
+        return BadRequest("Problem updating product");
     }
 
     [HttpDelete("{id:int}")]
     public async Task<ActionResult> DeleteProduct (int id){
-        var product = context.Products.Find(id);
+        var product = await repo.GetProductByIdAsync(id);
         if(product ==null) return NotFound();
-        context.Remove(product);
-        await context.SaveChangesAsync();
-        return NoContent();
+        repo.DeleteProduct(product);
+        if(await repo.SaveChangesAsync()){
+            return NoContent();
+        }
+        return BadRequest("Problem deleting product.");
+    }
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands(){
+        return Ok(await repo.GetBrandsAsync());
+    }
+       [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes(){
+        return Ok(await repo.GetTypesAsync());
     }
     private bool ProductExists(int id){
-        return context.Products.Any(x => x.Id==id);
+        return repo.ProductExists(id);
     }
 }
--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductsRepository>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
@@ -18,5 +21,17 @@ var app = builder.Build();
 
 
 app.MapControllers();
+
+try{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(System.Exception ex){
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
